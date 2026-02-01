f707c21b3062ded1df2074b1a26fc4ada89865c2
Victor <victorzallas165@gmail.com>
2026-02-01 11:07:11 -0600

    practica0.java

---
+++ mipaquete/practica0.java
@@ -0,0 +1,16 @@
+package mipaquete;
+ import java.util.Sacnner;
+import java.util.Scanner;
+public class Practica0{
+ Static Sacnner = new Scanner(System.in);
+Static void run(){
+    string nombre;
+    
+
+
+
+
+}
+public static void main (String) arg[]){
+run();
+}}
